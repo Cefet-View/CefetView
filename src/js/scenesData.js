adc9cc377f1fb3eac36b220d2a3aa80546eedd71
JuanCanle <amykappa570@gmail.com>
2025-04-27 13:54:11 -0300

    Arrumando as pastar de imagens

--- src/js/scenesData.js
+++ src/js/scenesData.js
@@ -3,37 +3,37 @@ import * as THREE from "three";
 // Definição dos cenários
 export const scenesData = {
   d308: {
-    image: "/images/D_308.jpg",
+    image: "/images/Bloco_D/Andar_3/D_308.jpg",
     hotspot: {
-      // Definição dos hotspots
-      image: "/images/hotspot.png",
+      image: "/images/Utilitários/hotspot.png",
       position: new THREE.Vector3(-30, -2, 6.5),
       scale: new THREE.Vector3(0.12, 0.12, 1),
       targetScene: "d208",
     },
   },
   d208: {
-    image: "/images/D_208.jpg",
+    image: "/images/Bloco_D/Andar_3/D_208.jpg",
     hotspot: [
       {
-        image: "/images/hotspot.png",
+        image: "/images/Utilitários/hotspot.png",
         position: new THREE.Vector3(-5, -5, -10),
         scale: new THREE.Vector3(0.1, 0.1, 1),
         targetScene: "ladoquadra",
       },
     ],
   },
+  // TODO tem que melhorar esse nome lado quadra é fodakkkk
   ladoquadra: {
-    image: "/images/lado_quadra.jpg",
+    image: "/images/Exterior/lado_quadra.jpg",
     hotspot: [
       {
-        image: "/images/hotspot.png",
+        image: "/images/Utilitários/hotspot.png",
         position: new THREE.Vector3(0, -5, -10),
         scale: new THREE.Vector3(0.1, 0.1, 1),
         targetScene: "d208",
       },
       {
-        image: "/images/hotspot.png",
+        image: "/images/Utilitários/hotspot.png",
         position: new THREE.Vector3(5, -5, -10),
         scale: new THREE.Vector3(0.1, 0.1, 1),
         targetScene: "d308",

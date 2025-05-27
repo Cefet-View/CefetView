716467e7f72796a974b5652ffd5bda7dd294e6c3
JuanCanle <amykappa570@gmail.com>
2025-05-26 21:56:03 -0300

    aprimorando design

--- src/components/initialPage.jsx
+++ src/components/initialPage.jsx
@@ -7,23 +7,20 @@ export default function InitialPage() {
   return (
     <div
       className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-        w-[90vw] h-[80vh]
-        bg-white/30 backdrop-blur-md rounded-3xl p-8 shadow-2xl
+        w-[80vw] h-[80vh]
+        bg-white/20 backdrop-blur-md rounded-3xl p-8 shadow-2xl
         flex flex-col justify-center items-center gap-10"
     >
       <h1
         className="text-8xl font-extrabold 
-        bg-clip-text text-transparent bg-gradient-to-br from-blue-500 to-blue-800"
+        bg-clip-text text-transparent bg-gradient-to-br from-blue-500 to-blue-900"
       >
         CEFET-VIEW
       </h1>
-
-      {/* Conteúdo centralizado na parte inferior */}
       <div className="flex flex-col items-center gap-8 mb-8">
-        {/* Botão Iniciar */}
         <button
-          className="bg-blue-700 hover:bg-blue-800 text-white font-extrabold text-4xl py-6 px-20 rounded-2xl shadow-md
-            transition duration-300 ease-in-out transform hover:scale-105 active:scale-95"
+          className="bg-blue-700  hover:bg-gray-100 text-white hover:text-blue-800 font-extrabold text-4xl py-6 px-20 rounded-2xl shadow-md
+            transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 delay-100"
         >
           INICIAR
         </button>

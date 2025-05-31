d836669e12f3083870ccaca7d811a1762fd24fca
JuanCanle <amykappa570@gmail.com>
2025-05-30 21:50:06 -0300

    Animação da tela inicial

--- src/components/initialPage.jsx
+++ src/components/initialPage.jsx
@@ -6,14 +6,14 @@ import { loadScene } from "../utils/sceneLoader";
 // Componentes
 
 export default function InitialPage() {
-  const [botaoSelecionado, setBotaoSelecionado] = useState("Inicial");
-
-  // TODO Fazer o botão de iniciar funcionar, atualmente ele não faz nada
-  
   // TODO Fazer com que ao clicar nos botões de cena, a cena seja carregada e a esfera atualizada com a textura correta
   // Atualmente os botões de cena apenas atualizam o estado do botão selecionado, mas não carregam a cena
   // Eu consegui trazer o sceneId para aqui porém o loadScene(sceneId, scene, setSphere, setHotspots) precisa desses outros parâmetros
   // Talvez seja necessário passar a cena, a esfera e os hotspots como props para o componente InitialPage ou usar um contexto global para gerenciar o estado da cena
+
+  const [botaoSelecionado, setBotaoSelecionado] = useState("Inicial");
+
+  const [show, setShow] = useState(true);
 
   const botoes = {
     Inicial: "entradaescola",
@@ -25,10 +25,16 @@ export default function InitialPage() {
 
   return (
     <div
-      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-        w-[80vw] h-[80vh]
+      className={`
+        ${
+          show
+            ? "-translate-y-1/2 opacity-100 scale-100"
+            : "-translate-y-[170%] opacity-50 scale-80"
+        }
+        absolute top-1/2 left-1/2 -translate-x-1/2 w-[80vw] h-[80vh]
         bg-white/20 backdrop-blur-md rounded-3xl p-8 shadow-2xl
-        flex flex-col justify-center items-center gap-10"
+        flex flex-col justify-center items-center gap-10
+        transition-all duration-1000 ease-in-out`}
     >
       <h1
         className="text-8xl font-extrabold
@@ -39,6 +45,9 @@ export default function InitialPage() {
 
       <div className="flex flex-col items-center gap-8 mb-8">
         <button
+          onClick={() => {
+            setShow(!show);
+          }}
           className="bg-blue-700 hover:bg-gray-100 text-white hover:text-blue-800 font-extrabold text-4xl py-6 px-20 rounded-2xl shadow-md
             transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 delay-100 hover:[transform:translateY(-.335rem)] hover:shadow-xl"
         >
